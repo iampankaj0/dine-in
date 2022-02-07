30970fe142f0cdd35dd90dc89b3c953e1af031aa
Pankaj Yadav <91059277+iampankaj0@users.noreply.github.com>
2022-02-07 18:15:54 +0530

    commit message

---
+++ src/container/AboutUs/AboutUs.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import { images } from '../../constants';
+
+import './AboutUs.css';
+
+const AboutUs = () => (
+  <div className='app__aboutus app__bg flex__center section__padding' id='about'>
+    <div className='app__aboutus-overlay flex__center'>
+      <img src={images.G} alt="g Latter" />
+    </div>
+
+    <div className="app__aboutus-content flex__center">
+      <div className="app__aboutus-content_about">
+        <h1 className="headtext__cormorant">
+          About Us
+        </h1>
+        <img src={images.spoon} alt="spoon" />
+        <p className="p__opensans">Sit Tellus Lobortis Sed Senectus Vivamus Molestie. Condimentum Volutpat Morbi Facilisis Quam Scelerisque Sapien, Et Penatibus Aliquam AMet Tellus.</p>
+        <button className="custom__button" type='button'>Know More</button>
+      </div>
+
+      <div className="app__aboutus-content_knife flex-center">
+        <img src={ images.knife } alt="knife" />
+      </div>
+
+
+      <div className="app__aboutus-content_history">
+        <h1 className="headtext__cormorant">
+          Our History
+        </h1>
+        <img src={images.spoon} alt="spoon" />
+        <p className="p__opensans">Sit Tellus Lobortis Sed Senectus Vivamus Molestie. Condimentum Volutpat Morbi Facilisis Quam Scelerisque Sapien, Et Penatibus Aliquam AMet Tellus.</p>
+        <button className="custom__button" type='button'>Know More</button>
+      </div>
+      
+
+    </div>
+
+
+
+  </div>
+);
+
+export default AboutUs;
